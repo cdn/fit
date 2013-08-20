fbb6ec14461d81dac57a675026fc0e93c4c337a8
Christopher Neale <cdn+github@chrs.nl>
2013-08-20 12:27:11 +0000

    move hashtag to configuration file

--- index.php
+++ index.php
@@ -253,7 +253,7 @@ for ($i=0; $i < 5; $i++) {
 	<article class="activity">
 		<div class="row">
 					<div class="small-6 large-10 columns">
-						<h4><?php echo $value -> type; ?> at <?php echo $da->format("l jS F Y \a\\t g:ia"); ?></h4>
+						<h4><?php echo $value -> type; ?> on <?php echo $da->format("l jS F Y \a\\t g:ia"); ?></h4>
 						<span>Distance: <?php echo round($value -> total_distance * $distance_convert, 2);
 							echo " ";
 							echo $distance_unit;
@@ -272,7 +272,7 @@ for ($i=0; $i < 5; $i++) {
 									<div class="small-6 large-10 columns">
 									<label>Message</label><br/>
 <textarea class="postContent">I&#8217;ve been <?php echo strtolower($value -> type); ?> - <?php echo "".round($value -> total_distance * $distance_convert, 2)." ".$distance_unit;?> in <?php echo unix2human(floor($value->duration));?>. <?php echo $value->total_calories;?> cal. See this on RunKeeper
- #addfitnesschallenge</textarea>
+ #<?php echo HASHTAG ?></textarea>
 <input class=linkAnnotation name=annolnk type=hidden value=<?php echo $profile_read->profile; ?>/activity/<?php echo str_ireplace("/fitnessActivities/", "", $value -> uri); ?>>
 </div>
 <div class="small-6 large-2 columns">
