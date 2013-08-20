c086a64e8226e2546a31d41d8975b75f6772cd41
Christopher Neale <cdn+github@chrs.nl>
2013-08-20 12:26:26 +0000

    add configuration file

---
+++ fitconfig.php
@@ -0,0 +1,11 @@
+<?php
+
+define('DIR', dirname(__FILE__) . '/');
+
+define('APPDOT_PATH', DIR);
+define('RK_PATH',     DIR);
+
+define('RK_API_YML', DIR . 'runkeeper/config/rk-api.yml');
+
+define('HASHTAG', 'FitnessChallenge');
+define('P_CHANNEL', 00000);
